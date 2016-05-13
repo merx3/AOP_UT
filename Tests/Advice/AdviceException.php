2853b15e6f70ef1ece96c388959121875d7c9725
Marian Marinov <mariyan.marinov@gfk.com>
2016-05-13 15:28:46 +0200

    Removing old folder

--- Tests/Advice/AdviceException.php
+++
@@ -1,8 +0,0 @@
-<?php namespace AOP_UT\Advice;
-
-class AdviceException extends \Exception
-{
-    public function __construct($message, $code = 0, Exception $previous = null) {
-        parent::__construct($message, $code, $previous);
-    }
-}
--- Tests/DAL/DataFlowDirection.php
+++
@@ -1,7 +0,0 @@
-<?php namespace AOP_UT\DAL;
-
-class DataFlowDirection
-{
-    const CALLING = 0;
-    const RETURNING = 1;
-}
