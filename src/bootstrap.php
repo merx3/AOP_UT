42713d17de8b0d86aedb84628c91769cbfc0792f
Marian Marinov <mariyan.marinov@gfk.com>
2016-05-13 15:34:56 +0200

    changing ini location

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$functionSignatures = parse_ini_file(__DIR__ .'/config/listener.ini')['signatures'];
+$functionSignatures = parse_ini_file(__DIR__ .'/../config/listener.ini')['signatures'];
 foreach ($functionSignatures as $func) {
     aop_add_around($func, array('AOP_UT\Advice\MethodsAdvice', 'adviceAround'));
 }
